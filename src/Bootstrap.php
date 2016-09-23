1d8f3ff2560923143cb0e1ea295c7b02d37c1b71
joseadame <joseadamefernandez@gmail.com>
2016-09-23 15:55:28 +0200

    Se añade el error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,2 +1,26 @@
 <?php
-echo 'Hello World!';
+namespace Example;
+
+$dir = dirname(_FILE_);
+require $dir .'/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$enviroment = 'development';
+
+/**
+* Registramos el error handler
+*/
+
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
